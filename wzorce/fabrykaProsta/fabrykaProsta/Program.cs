50410693ac6627a0fe3b3258dd405b85d95321cc
wardasz <m.wardzinski5991@gmail.com>
2019-04-24 10:20:16 +0200

    fabryki goto

--- wzorce/fabrykaProsta/fabrykaProsta/Program.cs
+++ wzorce/fabrykaProsta/fabrykaProsta/Program.cs
@@ -10,6 +10,28 @@ namespace fabrykaProsta
     {
         static void Main(string[] args)
         {
+            czolg.czolg poj1 = fabryka.dajFabryke().dajCzolg("Abrams");
+            Console.WriteLine(poj1.dajNazwe());
+            Console.WriteLine(poj1.dajPancerz());
+            bwp.bwp poj2 = fabryka.dajFabryke().dajBewupa("Warior");
+            Console.WriteLine(poj2.dajNazwe());
+            Console.WriteLine(poj2.dajKaliber());
+
+            licz(1000000);
+            Console.ReadKey();
+        }
+
+        static void licz(int x)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            DateTime poczatek = DateTime.Now;
+            for (int y = 0; y < x; y++)
+            {
+                czolg.czolg poj = fabryka.dajFabryke().dajCzolg("Abrams");
+            }
+            DateTime koniec = DateTime.Now;
+            Console.WriteLine("Czas trwania dla " + x + " obiektów wyniósł " + (koniec - poczatek));
         }
     }
 }
--- wzorce/fabrykaRefleksyjna/fabrykaProsta/Program.cs
+++ wzorce/fabrykaRefleksyjna/fabrykaProsta/Program.cs
@@ -4,22 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using metodaWytworcza;
-
-
-namespace metodaWytworcza
+namespace fabrykaRefleksyjna
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Ipojazd poj1 = fabrykaCzolgow.dajFabryke().stwoz("Abrams");
+            /*
+            var fabryczka = fabryka.dajFabryke();
+
+            fabryczka.twozenieSlownika();
+
+            czolg.czolg poj1 = fabryczka.zrobCzolg("Abrams");
+            poj1.dostan();
             poj1.ostrzelaj();
-            czolg.czolg poj2 = (czolg.czolg)fabrykaCzolgow.dajFabryke().stwoz("Abrams");
+            apc.apc poj2 = fabryczka.zrobTransporter("Rosomak");
             poj2.ostrzelaj();
-            poj2.dostan();
-            Ipojazd poj3 = fabrykaBwp.dajFabryke().stwoz("Darlo");
+            poj2.wyzadzDesant();
+            bwp.bwp poj3 = fabryczka.zrobBewupa("Darlo");
             poj3.ostrzelaj();
+            poj3.przełam();
+            dziala.dziala poj4 = fabryczka.zrobDzialo("Dana");
+            poj4.ostrzelaj();
+            poj4.przeladuj();
+            */
 
             licz(1000000);
             Console.ReadKey();
@@ -30,9 +38,10 @@ namespace metodaWytworcza
             Console.WriteLine();
             Console.WriteLine();
             DateTime poczatek = DateTime.Now;
+            fabryka.dajFabryke().twozenieSlownika();
             for (int y = 0; y < x; y++)
             {
-                Ipojazd poj = fabrykaCzolgow.dajFabryke().stwoz("Abrams");
+                czolg.czolg poj = fabryka.dajFabryke().zrobCzolg("Abrams");
             }
             DateTime koniec = DateTime.Now;
             Console.WriteLine("Czas trwania dla " + x + " obiektów wyniósł " + (koniec - poczatek));
