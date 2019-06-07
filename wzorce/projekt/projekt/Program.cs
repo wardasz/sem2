f804ce80705a6d3ba79a983cb589e8c146576fc0
wardasz <m.wardzinski5991@gmail.com>
2019-06-07 19:05:02 +0200

    wzorce projekt start

---
+++ wzorce/projekt/projekt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            loger l = loger.dajInstancje();
+            l.loguj("jeden");
+            loger j = loger.dajInstancje();
+            j.loguj("dwa");
+            bool czy = l.GetHashCode() == j.GetHashCode();
+            l.loguj(czy.ToString());
+        }
+    }
+}
---
+++ wzorce/projekt/projekt/loger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace projekt
+{
+    public class loger
+    {
+        private static loger instancja;
+
+        private loger() { }
+
+        public static loger dajInstancje()
+        {       
+            if (instancja == null)
+            {
+                instancja = new loger();
+            }
+            return instancja;
+        }
+
+        public void loguj(string a)
+        {
+            var s = new FileInfo(Directory.GetCurrentDirectory());
+            var s2 = s.Directory.Parent.Parent;
+            String sciezka = s2.ToString() + "\\rejestr.txt";
+
+            using (StreamWriter writer = new StreamWriter(sciezka, true))
+            {
+                writer.WriteLine(a);
+            }
+        }
+    }
+}
